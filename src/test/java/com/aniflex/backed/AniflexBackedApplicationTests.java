3a5bdebb51781bd67bd08be64821a94dfe977e61
ShervinDani <rvshervin@gmil.com>
2026-04-06 10:15:15 +0530

    Initial Commit

---
+++ src/test/java/com/aniflex/backed/AniflexBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniflex.backed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniflexBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
